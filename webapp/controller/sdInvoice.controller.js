7eb3c301e6fc77a2a63cb61dcf5e913d93df30c5
DineshGudapalli <gudapallid@londonhydro.com>
2025-07-29 17:10:13 +0530

    added pdf preview download

--- webapp/controller/sdInvoice.controller.js
+++ webapp/controller/sdInvoice.controller.js
@@ -21,6 +21,7 @@ sap.ui.define([
             oPDFModel = oController.getOwnerComponent().getModel("PDFPreviewService");
             oRouter = UIComponent.getRouter();
             var oSDInvoiceModel = new JSONModel({
+                oInvoicePDF: "",
                 SDInvoice: []
             });
             oController.getView().setModel(oSDInvoiceModel, "SDInvoiceModel");
@@ -67,7 +68,7 @@ sap.ui.define([
             var oJsonModel = new sap.ui.model.json.JSONModel();
             var oSDInvoiceModel = oController.getView().getModel("SDInvoiceModel");
             var aFilter = [];
-            const contractAccount = this.getView().byId("idCA").getValue();           
+            const contractAccount = this.getView().byId("idCA").getValue();
 
             if (contractAccount !== "") {
                 aFilter.push(new Filter("vkont", FilterOperator.EQ, contractAccount));
@@ -84,10 +85,10 @@ sap.ui.define([
                 success: function (response) {
                     oBusyDialog.close();
                     if (response.results.length > 0) {
-                        oSDInvoiceModel.setProperty("/SDInvoice", response.results);                       
+                        oSDInvoiceModel.setProperty("/SDInvoice", response.results);
                     }
                     else if (response.results.length === 0) {
-                        oSDInvoiceModel.setProperty("/SDInvoice", {});                        
+                        oSDInvoiceModel.setProperty("/SDInvoice", {});
                         return MessageBox.error("There are no records with selection.")
                     }
                 },
@@ -106,38 +107,48 @@ sap.ui.define([
         },
         onPressInvoice: function (oEvent) {
             debugger;
-            let oSource = oEvent.getSource();
-            let oInvoiceNo = oSource.getText();
+            let oSourceValue = oEvent.getSource();
+            let oInvoiceNo = oSourceValue.getText();
             if (oInvoiceNo) {
-                //var oModel = oController.getOwnerComponent.getModel("PDFPreviewService");
-                var sEntityPath = "/Print_previewSet('" + oInvoiceNo + "')/$value";
-                oPDFModel.read(sEntityPath, {
-                    success: function (oData, oResponse) {
-                        debugger;
-                        var sContentType = oResponse.headers["content-type"] || "application/pdf";
-                        var sBase64Data = oData;
-                        var sFileName = oResponse.headers["content-disposition"]?.match(/filename="(.+)"/)?.[1] || "document.pdf";
-                        var decodedPdfContent = atob(sBase64Data);
-                        var byteArray = new Uint8Array(decodedPdfContent.length);
-                        for (var i = 0; i < decodedPdfContent.length; i++) {
-                            byteArray[i] = decodedPdfContent.charCodeAt(i);
-                        }
-                        var oBlob = new Blob([byteArray.buffer], { type: sContentType });
-                        var sPdfUrl = URL.createObjectURL(oBlob);
-                        URLWhitelist.add("blob");
+                var oSource = "/sap/opu/odata/SAP/ZBI_PRINT_PREVIEW_SRV/Print_previewSet('" + oInvoiceNo + "')/$value";
+                this.getView().getModel("SDInvoiceModel").setProperty("/oInvoicePDF", oSource);
+                if (!this.oPdfDialog) {
+                    this.oPdfDialog = sap.ui.xmlfragment("com.sap.lh.mr.zlhsdinvoice.fragment.invoicePDF", this);
+                    this.getView().addDependent(this.oPdfDialog);
+                }
+                this.oPdfDialog.open();
 
-                        var oPDFViewer = new PDFViewer({
-                            source: sPdfUrl,
-                            title: sFileName,
-                            height: "600px"
-                        })
-                        oPDFViewer.open();
-                    }.bind(oController),
-                    error: function (oError) {
-                        MessageBox.error("Error loading PDF: ", oError.message);
-                    }
-                })
+                // var sEntityPath = "/Print_previewSet('" + oInvoiceNo + "')/$value";
+                // oPDFModel.read(sEntityPath, {
+                //     success: function (oData, oResponse) {
+                //         debugger;
+                //         var sContentType = oResponse.headers["content-type"] || "application/pdf";
+                //         var sBase64Data = oData;
+                //         var sFileName = oResponse.headers["content-disposition"]?.match(/filename="(.+)"/)?.[1] || "document.pdf";
+                //         var decodedPdfContent = atob(sBase64Data);
+                //         var byteArray = new Uint8Array(decodedPdfContent.length);
+                //         for (var i = 0; i < decodedPdfContent.length; i++) {
+                //             byteArray[i] = decodedPdfContent.charCodeAt(i);
+                //         }
+                //         var oBlob = new Blob([byteArray.buffer], { type: sContentType });
+                //         var sPdfUrl = URL.createObjectURL(oBlob);
+                //         URLWhitelist.add("blob");
+
+                //         var oPDFViewer = new PDFViewer({
+                //             source: sPdfUrl,
+                //             title: sFileName,
+                //             height: "600px"
+                //         })
+                //         oPDFViewer.open();
+                //     }.bind(oController),
+                //     error: function (oError) {
+                //         MessageBox.error("Error loading PDF: ", oError.message);
+                //     }
+                // })
             }
-        }
+        },
+        onCloseDialogPDF: function () {
+            this.oPdfDialog.close();
+        },
     });
 });
